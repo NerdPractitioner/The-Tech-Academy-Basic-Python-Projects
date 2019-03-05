7565844bfbf1c67a1a098cb97c201e438558084b
Matthew Holmes <mlholmes91@gmail.com>
2019-03-04 18:13:58 -0800

    Delete unnecessary file

--- Drill2/a.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('drill2.db')
-with conn:
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_drill_files( \
-        file_id INTEGER PRIMARY KEY AUTOINCREMENT, \
-        file_name TEXT \
-        )")
