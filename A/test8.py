e41c7ba921d51c3c612cd6fbd230612d4b7bdbb7
Matthew H <mlholmes91@gmail.com>
2019-03-10 13:57:35 -0700

    Upload practice files

--- A/test8.py
+++ A/test8.py
@@ -31,8 +31,7 @@ with conn:
     conn.commit()
 conn.close()
 
-
-con = conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('test.db')
 with conn:
     cur = conn.cursor()
     cur.execute("SELECT col_fname,col_lname,col_email FROM tbl_persons WHERE col_fname = 'Sarah'")
@@ -40,4 +39,3 @@ with conn:
     for item in varPerson:
         msg = "First Name: {}\nLast Name: {}\nEmail: {}".format(item[0],item[1],item[2])
     print(msg)
-
